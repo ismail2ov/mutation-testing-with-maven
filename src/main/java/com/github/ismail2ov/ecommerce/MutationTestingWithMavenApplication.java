861882394c7406c5533677d78d2c21c2b6c55e0d
Ismail Ahmedov <ismail2ov@gmail.com>
2025-09-01 00:31:20 +0200

    Apply spotless

--- src/main/java/com/github/ismail2ov/ecommerce/MutationTestingWithMavenApplication.java
+++ src/main/java/com/github/ismail2ov/ecommerce/MutationTestingWithMavenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MutationTestingWithMavenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MutationTestingWithMavenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MutationTestingWithMavenApplication.class, args);
+    }
 
 }
--- src/test/java/com/github/ismail2ov/ecommerce/MutationTestingWithMavenApplicationTests.java
+++ src/test/java/com/github/ismail2ov/ecommerce/MutationTestingWithMavenApplicationTests.java
@@ -1,15 +1,16 @@
 package com.github.ismail2ov.ecommerce;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import org.junit.jupiter.api.Test;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class MutationTestingWithMavenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
